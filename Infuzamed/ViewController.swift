8d8882f827efd6d4139afd93cffe90ca53e72da5
Vazgen <vazgen@trebel.io>
2023-07-13 21:06:31 +0400

    Initial Commit

---
+++ Infuzamed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Infuzamed
+//
+//  Created by Vazgen on 7/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
